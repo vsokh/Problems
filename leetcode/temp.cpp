abe5626db265f7baa83a70d5046f2215c5f1b6a7
vsokh <vsokolohorskyi@gmail.com>
2021-07-07 22:39:47 +0300

    Updated template

--- leetcode/temp.cpp
+++ leetcode/temp.cpp
@@ -1,66 +1,58 @@
 #include <bits/stdc++.h>
-/* #define e9 1e9 */
-
-/* #define setp(x) fixed<<setprecision(x) */
-
-/* #define ulmx LLONG_MAX */
-/* #define lmx LLONG_MAX */
-/* #define lmn LLONG_MIN */
-
-/* template<typename T> */
-/* struct number_iterator : std::iterator<random_access_iterator_tag, T> { */
-/* 	T v; */
-/* 	number_iterator(T _v) : v(_v) {} */
-/* 	operator T&(){return v;} */
-/* 	T operator *() const {return v;} */
-/* }; */
-
-/* template<typename T> */
-/* struct number_range { */
-/* 	T b,e; */
-/* 	number_range(T b, T e):b(b),e(e){} */
-/* 	number_iterator<T> begin(){return b;} */
-/* 	number_iterator<T> end(){return e;} */
-/* }; */
-
-/* template<typename T> number_range<T> range(T e) {return number_range<T>(0, e);} */
-/* template<typename T> number_range<T> range(T b, T e) {return number_range<T>(b, e);} */
-
-/* template<typename... Args> */
-/* auto Func(Args... args) */
-/* { */
-/*     return (args + ...); */
-/* } */
-
-/* void err(istream_iterator<string> it) {} */
-/* template<typename T, typename... Args> */
-/* void err(istream_iterator<string> it, T a, Args... args) { */
-/* 	cerr << *it << " = " << a << endl; */
-/* 	err(++it, args...); */
-/* } */
-
-/* #define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); } */
 
 using namespace std;
 
+template<typename T>
+struct number_iterator : std::iterator<random_access_iterator_tag, T> {
+	T v;
+	number_iterator(T _v) : v(_v) {}
+	operator T&(){return v;}
+	T operator *() const {return v;}
+};
+
+template<typename T>
+struct number_range {
+	T b,e;
+	number_range(T b, T e):b(b),e(e){}
+	number_iterator<T> begin(){return b;}
+	number_iterator<T> end(){return e;}
+};
+
+template<typename T> number_range<T> range(T e) {return number_range<T>(0, e);}
+template<typename T> number_range<T> range(T b, T e) {return number_range<T>(b, e);}
+
+	template<typename... Args>
+auto Func(Args... args)
+{
+	return (args + ...);
+}
+
+void err(istream_iterator<string> it) {}
+
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+#define e9 1e9
 #define all(x) begin(x), end(x)
 #define sz(x) (ll)(x).size()
 #define nl "\n"
-
-#define ull unsigned long long
-#define ll long long
 
 #define f first
 #define s second
 #define mp make_pair
 #define pb push_back
+#define pf push_front
 
-#define yes cout<<"YES"<<endl
-#define no cout<<"NO"<<endl
-
-#define vl vector<ll>
-#define sl set<ll>
-#define pl pair<ll, ll>
+#define vi vector<int>
+#define li list<int>
+#define si set<int>
+#define pi pair<int, int>
 
 void setIO(string name = "")
 {
@@ -71,9 +63,12 @@ void setIO(string name = "")
 	}
 }
 
-void solve()
-{
-}
+
+
+class Solution {
+	public:
+
+};
 
 int main()
 {
