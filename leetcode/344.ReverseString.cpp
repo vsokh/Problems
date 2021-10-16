1a8aeec8174dfd31b3f74ebb10b7e5c66526cef9
vsokh <vsokolohorskyi@gmail.com>
2021-10-16 18:07:38 +0300

    Solve 344.ReverseString

---
+++ leetcode/344.ReverseString.cpp
@@ -0,0 +1,92 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename T>
+struct number_iterator : std::iterator<random_access_iterator_tag, T> {
+	T v;
+	number_iterator(T _v) : v(_v) {}
+	operator T&(){return v;}
+	T operator *() const {return v;}
+};
+
+template<typename T>
+struct number_range {
+	T b,e;
+	number_range(T b, T e):b(b),e(e){}
+	number_iterator<T> begin(){return b;}
+	number_iterator<T> end(){return e;}
+};
+
+template<typename T> number_range<T> range(T e) {return number_range<T>(0, e);}
+template<typename T> number_range<T> range(T b, T e) {return number_range<T>(b, e);}
+
+	template<typename... Args>
+auto Func(Args... args)
+{
+	return (args + ...);
+}
+
+void err(istream_iterator<string> it) {}
+
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args)
+{
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
+
+#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+#define e9 1e9
+#define all(x) begin(x), end(x)
+#define sz(x) (ll)(x).size()
+#define nl "\n"
+
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+
+#define vi vector<int>
+#define li list<int>
+#define si set<int>
+#define pi pair<int, int>
+
+void setIO(string name = "")
+{
+	cin.tie(0)->sync_with_stdio(0);
+	if (name.size()) {
+		freopen((name+".in").c_str(), "r", stdin);
+		freopen((name+".out").c_str(), "w", stdout);
+	}
+}
+
+class Solution
+{
+public:
+    void reverseString(vector<char>& s)
+	{
+		auto it1 = s.begin();
+		auto it2 = s.end(); --it2;
+		while (it1 < it2)
+		{
+			std::swap(*it1, *it2);
+			++it1; --it2;
+		}
+    }
+};
+
+int main()
+{
+	Solution s;
+	vector<char> str = {'h','e','l','l','o'};
+	s.reverseString(str);
+
+	for (auto c : str)
+		cout << c << " ";
+	cout << endl;
+	return 0;
+}
