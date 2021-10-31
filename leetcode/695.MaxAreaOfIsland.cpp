6851cbdcf461a04e2ecec6a611d94eb99c02ee17
vsokh <vsokolohorskyi@gmail.com>
2021-10-31 08:11:30 +0200

    Add 695

--- leetcode/695.MaxAreaOfIsland.cpp
+++ leetcode/695.MaxAreaOfIsland.cpp
@@ -1,69 +1,61 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define all(x) begin(x), end(x)
-#define sz(x) (ll)(x).size()
-#define nl "\n"
-
-#define ull unsigned long long
-#define ll long long
-
-#define f first
-#define s second
-#define mp make_pair
-#define pb push_back
-
-#define yes cout<<"YES"<<endl
-#define no cout<<"NO"<<endl
-
-#define vl vector<ll>
-#define sl set<ll>
-#define pl pair<ll, ll>
-
-void setIO(string name = "")
-{
-	cin.tie(0)->sync_with_stdio(0);
-	if (name.size()) {
-		freopen((name+".in").c_str(), "r", stdin);
-		freopen((name+".out").c_str(), "w", stdout);
-	}
-}
-
 class Solution {
-public:
-	void countArea(vector<vector<int>> &G, vector<vector<int>> &grid, int i, int j, int &area) {
-		if(i<0||i>=grid.size())return;
-		if(j<0||j>=grid[i].size())return;
-		if(!grid[i][j])return;
-		if(G[i][j])return;
 
-		++area;
-		G[i][j]=1;
-		countArea(G, grid, i, j-1, area);
-		countArea(G, grid, i, j+1, area);
-		countArea(G, grid, i-1, j, area);
-		countArea(G, grid, i+1, j, area);
-	}
-    int areaOfIsland(vector<vector<int>>& grid) {
-		int maxArea=0;
-		vector<vector<int>> G;
-		for(int i=0;i<grid.size();++i)
-			for(int j=0;j<grid[i].size();++j)
-				G[i][j]=0;
-		for(int i=0;i<grid.size();++i){
-			for(int j=0;j<grid[i].size();++j){
-				int currArea=0;
-				countArea(G, grid, i, j, currArea);
-				maxArea=max(maxArea, currArea);
-			}
-		}
-		return maxArea;
+public:
+    void dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, int r, int c, int& m)
+    {
+        if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size())
+            return;
+        if (grid[r][c]==0)
+            return;
+        if (visited[r][c]==1)
+            return;
+
+        visited[r][c]=1; ++m;
+        dfs(grid, visited, r-1, c, m);
+        dfs(grid, visited, r, c-1, m);
+        dfs(grid, visited, r+1, c, m);
+        dfs(grid, visited, r, c+1, m);
+
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid)
+    {
+        vector<vector<int>> visited(grid.size(), vector<int>(grid[0].size()));
+        int m = 0;
+        for (int r = 0; r < grid.size(); ++r)
+        {
+            for (int c = 0; c < grid[0].size(); ++c)
+            {
+                int tmp = 0;
+                if (visited[r][c]==0)
+                {
+                    dfs(grid, visited, r, c, tmp);
+                }
+                m=max(m,tmp);
+            }
+        }
+        return m;
     }
 };
 
-
 int main()
 {
-	return 0;
+    vector<vector<int>> v = {
+        {0,0,1,0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+        {0,1,1,0,1,0,0,0,0,0,0,0,0},
+        {0,1,0,0,1,1,0,0,1,0,1,0,0},
+        {0,1,0,0,1,1,0,0,1,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0,1,0,0},
+        {0,0,0,0,0,0,0,1,1,1,0,0,0},
+        {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+    Solution s;
+    cout << "Exptected: 6" << endl;
+    cout << "Actual: " << s.maxAreaOfIsland(v) << endl;
+    return 0;
 }
