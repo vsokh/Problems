f4f50f6e3d3b2d67fd3d88c4b9a8fd24a6457d9c
vsokh <vsokolohorskyi@gmail.com>
2022-01-09 23:11:12 +0200

    Tried...

---
+++ codeforces/763/A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+using vi = vector<int>;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    int t; t << cin;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
